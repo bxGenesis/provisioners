f098f93a7a2d152a0fb388d83d174fa15961b85d
Mohsen-Bisp0017 BANAN <form@mohsen.banan.1.byname.net>
2020-06-20 19:27:33 -0700

    better way of determining rootDirProvisioners.

--- bin/bisosProvisioners_lib.sh
+++ bin/bisosProvisioners_lib.sh
@@ -10,6 +10,18 @@ _EOF_
     
     local extraInfo="$1"
 
+  cat  << _EOF_
+$( examplesSeperatorChapter "Development Feature:: Git Enable, Activate, Prep" )
+${G_myName} ${extraInfo} -i gitReposReport
+$( examplesSeperatorSection "Git Auth (Development) Setups" )
+${G_myName} ${extraInfo} -i gitPrepAuth
+${G_myName} ${extraInfo} -i gitActivateAuth
+${G_myName} ${extraInfo} -i gitEnableAuth
+$( examplesSeperatorSection "Git Anon (Released) Setups" )
+${G_myName} ${extraInfo} -i gitPrepAnon
+${G_myName} ${extraInfo} -i gitActivateAnon
+_EOF_
+    
     cat  << _EOF_
 $( examplesSeperatorChapter "BISOS Provisioning:: Selfcontained ICMs Invocations" )
 $( examplesSeperatorSection "Create Accounts" )
@@ -20,6 +32,116 @@ $( examplesSeperatorSection "Create /bisos Bases" )
 ${G_myName} ${extraInfo} -i bisosBaseDirsSetup
 _EOF_
 }
+
+
+function vis_gitReposReport {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+If needed, git auth clone.
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+
+    #local selfcontainedBase=$( vis_basedOnGitDetermineThisSelfcontainedBase 2> /dev/null )
+    local selfcontainedBase=$( vis_rootDirProvisionersGet 2> /dev/null )    
+    local gitReposBase="${selfcontainedBase}/gitRepos"    
+
+    lpDo ls -l "${gitReposBase}"/*
+    
+    lpReturn
+}
+
+
+function vis_gitPrepAuth {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+Git Auth clone/update and activate.
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+
+    lpDo vis_gitEnableAuth
+
+    lpDo vis_gitActivateAuth    
+    
+    lpReturn
+}
+
+
+function vis_gitEnableAuth {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+If needed, git auth clone.
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+
+    local selfcontainedBase=$( vis_basedOnGitDetermineThisSelfcontainedBase 2> /dev/null )
+    local gitReposAuthBase="${selfcontainedBase}/gitReposAuth"
+    local thisRepoDir="${gitReposAuthBase}/provisioners"
+
+    lpDo mkdir -p "${gitReposAuthBase}"
+
+    if [ -d "${thisRepoDir}" ] ; then
+	ANT_raw "${thisRepoDir} exists -- git clone skipped"
+    else
+	inBaseDirDo "${gitReposAuthBase}" git clone git@github.com:bxGenesis/provisioners.git
+    fi
+
+    lpReturn
+}
+
+function vis_gitActivateAuth {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+Update the symlink from gitRepos to point to gitReposAuth.
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+
+    local selfcontainedBase=$( vis_basedOnGitDetermineThisSelfcontainedBase 2> /dev/null )
+    local gitReposAuthBase="${selfcontainedBase}/gitReposAuth"
+    local gitReposBase="${selfcontainedBase}/gitRepos"    
+
+    opDo FN_fileSymlinkUpdate "${gitReposAuthBase}/provisioners" "${gitReposBase}/provisioners" 
+
+    lpReturn
+}
+
+
+function vis_gitPrepAnon {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+Git Auth clone/update and activate.
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+
+    lpDo vis_gitActivateAnon    
+
+    lpReturn
+}
+
+
+function vis_gitActivateAnon {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+Update the symlink from gitRepos to point to gitReposAnon.
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+
+    local selfcontainedBase=$( vis_basedOnGitDetermineThisSelfcontainedBase 2> /dev/null )
+    local gitReposAnonBase="${selfcontainedBase}/gitReposAnon"
+    local gitReposBase="${selfcontainedBase}/gitRepos"    
+
+    opDo FN_fileSymlinkUpdate "${gitReposAnonBase}/provisioners" "${gitReposBase}/provisioners" 
+
+    lpReturn
+}
+
+
+
 
 function provisionersBinBaseGet {
     G_funcEntry
--- bin/sharedParameters_lib.sh
+++ bin/sharedParameters_lib.sh
@@ -9,6 +9,25 @@ provisionersBaseDir="/opt/bisosProvisioner"
 venvBasePy2="${provisionersBaseDir}/venv/py2"
 venvBasePy3="${provisionersBaseDir}/venv/py3"    
 
+
+function vis_rootDirProvisionersGet {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+echo someParam and args 
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+
+    local rootDir_provisioners=$( bx-platformInfoManage.py  -i pkgInfoParsGet | grep rootDir_provisioners | cut -d '=' -f 2 )
+
+    if [ -z "${rootDir_provisioners}" ] ; then
+	EH_problem "Missing rootDir_provisioners in bx-platformInfoManage.py"
+    fi
+    echo "${rootDir_provisioners}"
+}
+
+
+
 function vis_basedOnGitDetermineThisSelfcontainedBase {
     G_funcEntry
     function describeF {  G_funcEntryShow; cat  << _EOF_
